2f5ff8058c637721ee71d87a38ff07c3b68a84f4
james-harper <jameswilliamharper@gmail.com>
2019-05-27 22:48:06 +0100

    Output 'Sleep' for multiples of 5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import Constants from './constants';
 
 export const convert = (number) => {
+  if (number % 5 === 0) {
+    return Constants.SLEEP;
+  }
+
   if (number % 3 === 0) {
     return Constants.LEESA;
   }
--- src/index.test.js
+++ src/index.test.js
@@ -3,7 +3,7 @@ import expect from 'expect';
 import { convert } from '.';
 import Constants from './constants';
 
-describe('fizz buzz test', () => {
+describe('fizz buzz tests', () => {
   it('outputs the number as is, if it is not a multiple of 3 or 5', () => {
     expect(convert(1)).toBe(1);
     expect(convert(2)).toBe(2);
@@ -13,5 +13,10 @@ describe('fizz buzz test', () => {
   it('outputs "Leesa" if the number is a multiple of 3', () => {
     expect(convert(3)).toBe(Constants.LEESA);
     expect(convert(9)).toBe(Constants.LEESA);
+  });
+
+  it('outputs "Sleep" if the number is a multiple of 5', () => {
+    expect(convert(5)).toBe(Constants.SLEEP);
+    expect(convert(50)).toBe(Constants.SLEEP);
   });
 });
